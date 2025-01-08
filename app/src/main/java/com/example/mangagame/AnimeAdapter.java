46b634777187ab730973d6a7778e80785b13fd18
nayrariosaguilar <nayrios2004@gmail.com>
2025-01-08 18:07:46 +0100

    mejor formado

--- app/src/main/java/com/example/mangagame/AnimeAdapter.java
+++ app/src/main/java/com/example/mangagame/AnimeAdapter.java
@@ -4,13 +4,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
 
 public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.AnimeViewHolder> {
     private List<Anime> items;
+
+    //instanciamos los campos de mi anime_row,
+    //tiene en memoria cada pieza
     public static class AnimeViewHolder extends RecyclerView.ViewHolder {
         public ImageView imagen;
         public TextView nombre;
@@ -18,6 +19,7 @@ public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.AnimeViewHol
 
 
        //mete dentro del array los valores adecuados
+        //les abre el findById
         public AnimeViewHolder(View v) {
             super(v);
             imagen = (ImageView) v.findViewById(R.id.imagen);
@@ -43,11 +45,12 @@ public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.AnimeViewHol
         return new AnimeViewHolder(v);
     }
 
-    //
+    //vinculas los elementos con la posicion que estan en la list del main
     @Override
     public void onBindViewHolder(AnimeViewHolder viewHolder, int i) {
         viewHolder.imagen.setImageResource(items.get(i).getImagen());
+        //de la lista de items obten el que esta en la position
         viewHolder.nombre.setText(items.get(i).getNombre());
-        viewHolder.visitas.setText("Visitas:"+String.valueOf(items.get(i).getVisitas()));
+        viewHolder.visitas.setText("Visitas:"+(items.get(i).getVisitas()));
     }
 }
--- app/src/main/java/com/example/mangagame/MainActivity.java
+++ app/src/main/java/com/example/mangagame/MainActivity.java
@@ -12,33 +12,40 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recycler;
-    private RecyclerView.Adapter adapter;
+  //  private RecyclerView.Adapter adapter;
+    private AnimeAdapter adapter;
+    private   List items = new ArrayList();
     private RecyclerView.LayoutManager lManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List items = new ArrayList();
-
-        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
-        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
-        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
-        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
-        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
-        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
-        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
-        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
-        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
-        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
-        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
-        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
+        inicioListAnimes();
+        inicioElementos();
+    }
+    private void inicioElementos() {
 
         recycler = (RecyclerView) findViewById(R.id.recyclerView);
         recycler.setHasFixedSize(true);
         lManager = new LinearLayoutManager(this);
         recycler.setLayoutManager(lManager);
-        adapter = new AnimeAdapter(items);
         recycler.setAdapter(adapter);
+        adapter = new AnimeAdapter(items);
+    }
+
+    private void inicioListAnimes() {
+        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
+        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
+        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
+        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
+        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
+        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
+        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
+        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
+        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
+        items.add(new Anime(R.drawable.fate, "Fate Stay Night", 342));
+        items.add(new Anime(R.drawable.angelbeats, "Welcome to the NHK", 645));
+        items.add(new Anime(R.drawable.deathnote, "Suzumiya Haruhi", 459));
 
     }
 }
